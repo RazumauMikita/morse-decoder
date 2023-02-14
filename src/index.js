e6756f8f166d378b59f366e6ebfd82e415a09a09
Mikita <anykey2solo@gmail.com>
2023-02-14 18:18:55 +0300

    feat: commit

--- src/index.js
+++ src/index.js
@@ -38,8 +38,53 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr = expr.split('**********');
+    let all = [];
+    for(let i = 0; i < arr.length; i++) {
+     
+     let numSymb = arr[i].length / 10;
+     let leng = arr[i].length;
+     let comon = [];
+     let str = '';
+   
+         
+         for(let j = 0, m = 0; m < numSymb; m++) {
+             str = '';
+ 
+             for (; ;) {
+ 
+              if(arr[i][leng - 1 - (j + 1)] + arr[i][leng - 1 - j] === '11') {
+                     str = str + '-';
+                 }
+             if(arr[i][leng - 1 - (j + 1)] + arr[i][leng - 1 - j] === '10') {
+                     str = str + '.';
+ 
+                 }
+             if(arr[i][leng - 1 - (j + 1)] + arr[i][leng - 1 - j] === '00') {
+                 }
+                 j = j + 2;
+             if (j % 10 === 0) break;
+             }
+             comon.push(str.split('').reverse().join(''));
+         };
+ 
+         let neo = comon.reverse();
+ 
+ 
+         let res = [];
+       
+         
+         let mors = '';
+         for(let i = 0; i < neo.length; i++) {
+             mors = mors + MORSE_TABLE[neo[i]];
+         }
+   
+         all.push(mors);
+    }
+ 
+    return all.join(' ');
+ };
+
 
 module.exports = {
     decode
